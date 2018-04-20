b2b254a11fa72b07361073553e5c761e7ed0862c
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2018-04-20 11:40:02 +0800

    Remove commented code

--- server.js
+++ server.js
@@ -15,11 +15,6 @@ app.use(compression()); // gzip
 // Serve statically the dist/ directory (build artefact)
 app.use(express.static(distDir));
 
-// // Serve elm index.html
-// app.get( '/', ( req, res ) => {
-// 	res.sendFile( path.join( distDir, 'index.html' ) )
-// })
-
 // Start server
 app.listen(port, () => {
   console.log("Server listening on port %s", port);

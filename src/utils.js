91933407954055e6c5b058939e5624c07b5c34ef
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2018-04-22 02:31:06 +0800

    All MPL-2.0 license to ports.js and utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
 const utils = (function() {
   // Download a file through an href temporary DOM element.
   // example use: download( str, "selection.json", "text/plain" )
